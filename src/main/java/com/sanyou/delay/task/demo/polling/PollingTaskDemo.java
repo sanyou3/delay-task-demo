a57cf0c8302ff35fd2a333dac6bcce159dacc6b8
yisuan <yisuan@uni-ubi.com>
2023-02-27 20:13:49 +0800

    初始化

---
+++ src/main/java/com/sanyou/delay/task/demo/polling/PollingTaskDemo.java
@@ -0,0 +1,59 @@
+package com.sanyou.delay.task.demo.polling;
+
+import com.sanyou.delay.task.demo.jdk.DelayQueueDemo;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * TODO
+ *
+ * @author yisuan
+ * @date 2023/2/27 07:34
+ */
+@Slf4j
+public class PollingTaskDemo {
+
+    private static final List<DelayTask> DELAY_TASK_LIST = new CopyOnWriteArrayList<>();
+
+    public static void main(String[] args) {
+        new Thread(() -> {
+            while (true) {
+                try {
+                    for (DelayTask delayTask : DELAY_TASK_LIST) {
+                        if (delayTask.triggerTime <= System.currentTimeMillis()) {
+                            log.info("处理延迟任务:{}", delayTask.taskContent);
+                            DELAY_TASK_LIST.remove(delayTask);
+                        }
+                    }
+                    TimeUnit.MILLISECONDS.sleep(100);
+                } catch (Exception e) {
+                }
+            }
+        }).start();
+
+        log.info("提交延迟任务");
+        DELAY_TASK_LIST.add(new DelayTask("三友的java日记", 5L));
+    }
+
+    @Getter
+    @Setter
+    public static class DelayTask {
+
+        private final String taskContent;
+
+        private final Long triggerTime;
+
+        public DelayTask(String taskContent, Long delayTime) {
+            this.taskContent = taskContent;
+            this.triggerTime = System.currentTimeMillis() + delayTime * 1000;
+        }
+    }
+
+}
---
+++ src/main/java/com/sanyou/delay/task/demo/qurtaz/QuartzDemo.java
@@ -0,0 +1,56 @@
+package com.sanyou.delay.task.demo.qurtaz;
+
+import cn.hutool.core.date.DateUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+
+import java.util.Date;
+
+/**
+ * TODO
+ *
+ * @author yisuan
+ * @date 2023/2/27 07:51
+ */
+@Slf4j
+public class QuartzDemo {
+
+    public static void main(String[] args) throws SchedulerException, InterruptedException {
+        // 1.创建Scheduler的工厂
+        SchedulerFactory sf = new StdSchedulerFactory();
+        // 2.从工厂中获取调度器实例
+        Scheduler scheduler = sf.getScheduler();
+
+        // 6.启动 调度器
+        scheduler.start();
+
+        // 3.创建JobDetail
+        JobDetail jb = JobBuilder.newJob(SanYouJob.class)
+                .usingJobData("delayTask", "这是一个延迟任务")
+                .build();
+
+        // 4.创建Trigger
+        Trigger t = TriggerBuilder.newTrigger()
+                //任务的触发时间就是延迟任务到的延迟时间
+                .startAt(DateUtil.offsetSecond(new Date(), 5))
+                .build();
+
+        // 5.注册任务和定时器
+        log.info("提交延迟任务");
+        scheduler.scheduleJob(jb, t);
+    }
+
+    @Slf4j
+    public static class SanYouJob implements Job {
+
+        @Override
+        public void execute(JobExecutionContext context) throws JobExecutionException {
+            JobDetail jobDetail = context.getJobDetail();
+            JobDataMap jobDataMap = jobDetail.getJobDataMap();
+            log.info("获取到延迟任务:{}", jobDataMap.get("delayTask"));
+        }
+
+    }
+
+}
